4e15d1272f0fafb1923d2d71352037e3b267e3f3
Paul Riley <paul@bigfatfrog.co.uk>
2020-09-08 12:59:31 +0100

    Prevent messages being created with 15 secs of each other

--- src/Services/MessageService.php
+++ src/Services/MessageService.php
@@ -13,26 +13,38 @@ use Doctrine\ORM\EntityManagerInterface;
 class MessageService
 {
 
-    public function __construct(EntityManagerInterface $entityManager,StatusRepository $statusRepository){
+    public function __construct(EntityManagerInterface $entityManager,
+                                StatusRepository $statusRepository,
+                                PredisService $predisService){
 
         $this->entityManager = $entityManager;
         $this->statusRepository = $statusRepository;
+        $this->predisService = $predisService;
     }
 
     public function createMessage(User $user, $phone, $text){
 
+        $datetime = new DateTime();
+
+        $lastDatetime = $this->predisService->get("user_".$user->getId());
+        $diff = strtotime($datetime->format('Y-m-d H:i:s')) - strtotime($lastDatetime);
+        if ($diff <= 15){
+            throw new \Exception("Messages within 15 secs");
+        }
         // check input before updating
-        if(strlen($text) <= 140 && strlen($text) > 0 && self::validatePhone($phone)){
+        else if(strlen($text) <= 140 && strlen($text) > 0 && self::validatePhone($phone)){
+
             $message = new Message();
             $message->setUser($user);
             $message->setPhone($phone);
             $message->setText($text);
             $message->setStatus($this->statusRepository->findOneBy(array('description' => 'awaiting')));
-            $message->setUpdatedAt(new DateTime());
+            $message->setUpdatedAt($datetime);
             $this->entityManager->persist($message);
             $this->entityManager->flush();
-
+            $this->predisService->set("user_".$user->getId(), $datetime->format('Y-m-d H:i:s'));
             return $message;
+
         } else {
             throw new \Exception("Invalid message input");
         }
--- tests/Services/MessageServiceTest.php
+++ tests/Services/MessageServiceTest.php
@@ -20,11 +20,16 @@ class MessageServiceTest extends KernelTestCase
 
     public function testCreateMessage()
     {
+        /*
+         *  Test create a standard message
+         */
         $user = $this->userRepository->findOneBy(array('username' => 'jim'));
         $phone = $_ENV['TWILLO_TEST_NO'];
         $text = 'hello';
         $message = $this->messageService->createMessage($user, $phone, $text);
         $this->assertEquals(true, $message instanceof Message);
+
+        sleep(16);
 
         $text = null;
         $exception = false;
@@ -34,6 +39,11 @@ class MessageServiceTest extends KernelTestCase
             $exception = true;
         }
         $this->assertEquals(true, $exception, "No text should have thrown an exception");
+
+        /*
+         *  Test min and max text validation
+         */
+        sleep(16);
 
         $text = "11111111111111111111" . "11111111111111111111" . "11111111111111111111" .
             "11111111111111111111" . "11111111111111111111" . "11111111111111111111" .
@@ -46,6 +56,8 @@ class MessageServiceTest extends KernelTestCase
         }
         $this->assertEquals(false, $exception, "140 chr text should NOT have thrown an exception");
 
+        sleep(16);
+
         $text .= "X";
         $exception = false;
         try {
@@ -54,6 +66,11 @@ class MessageServiceTest extends KernelTestCase
             $exception = true;
         }
         $this->assertEquals(true, $exception, "140 chr text should have thrown an exception");
+
+        /*
+         *  Test phone number validation
+         */
+        sleep(16);
 
         $text = "hello";
         $phone = 'rubbish';
@@ -64,5 +81,26 @@ class MessageServiceTest extends KernelTestCase
             $exception = true;
         }
         $this->assertEquals(true, $exception, "rubbish phone should have thrown an exception");
+
+        /*
+         *  Test 15 second message delay is working
+         */
+
+        $phone = $_ENV['TWILLO_TEST_NO'];
+        $text = 'hello';
+        $message = $this->messageService->createMessage($user, $phone, $text);
+        $this->assertEquals(true, $message instanceof Message);
+
+        sleep(16);
+
+        $text = null;
+        $exception = false;
+        try {
+            $message = $this->messageService->createMessage($user, $phone, $text);
+        } catch (\Exception $e) {
+            $exception = true;
+        }
+        $this->assertEquals(true, $exception,
+            "An exception should be thrown if the delay is less than 15 sec");
     }
 }
